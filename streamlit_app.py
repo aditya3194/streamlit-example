323b0b0782b5ad0ecfe05cc8a1dd9e735d5290e4
aditya3194 <adityamahajan6040@gmail.com>
2023-09-27 12:11:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,20 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from transformers import pipeline
 
-"""
-# Welcome to Streamlit!
+# Load the chatbot model
+model = pipeline("text-generation", model="cardiff-nlp/twitter-roberta-base-chatbot")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Define the banking dataset
+banking_data = ["What is the interest rate for a savings account?", "How do I deposit money into my account?", "What is the routing number for my bank?"]
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Define the chatbot UI
+ui = st.sidebar.select_option("Select a conversation topic", banking_data)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Start the chatbot
+st.sidebar.chatbot(model, ui)
 
+# Train the chatbot
+st.sidebar.train_chatbot(model, banking_data)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Run the chatbot
+st.sidebar.run_chatbot(model, ui)
